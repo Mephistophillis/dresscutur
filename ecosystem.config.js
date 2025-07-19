5f9fbd2563bd3ffc40c70a165e3cf72a3564ee08
Mephistophillis <woodenjester@gmail.com>
2025-07-19 15:35:28 +0300

    install deps fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -63,7 +63,7 @@ module.exports = {
       repo: 'git@github.com:your-username/dresscutur.git',
       path: '/var/www/dresscutur',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm ci && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
